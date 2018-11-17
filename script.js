d76ee9dc66fa9ee1b191a77e17ffbb5c0b342e9d
Nick Friedland <nick.friedland@gmail.com>
2018-11-16 17:13:30 -0800

    End of week

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ $(function() {
   //  TOGGLE FAVORITES STAR ON/OFF
   $('#all-articles-list').on('click', '.fa-star', function(e) {
     // how do you know which method to use? -- addFavorite vs. removeFavorite??
+    // Add/Remove favorites to user instance ownStories/favorites list??
     console.log(e.target);
 
     if ($(e.target).hasClass('far')) {
@@ -50,17 +51,18 @@ $(function() {
   });
 
   // SHOW/HIDE FAVORITES LIST
-  // $('#nav-favorites').on('click', 'ol-storiesList', function(){
-    // event delegation for parent of all stories li's
-    // In css, default display for far to display:block;
+  $('#nav-favorites').on('click', function() {
     // If far is currently display:block >>
-      // clicking on favorites id should add display: none / remove display:block
+    // clicking on faves # should invoke hide()/show() jQ methods as needed
     // else
-    // if()
+    console.log('Yay!');
+    $('.far')
+      .parent()
+      .toggle();
+    console.log('Also yay!');
+  });
 
-  // })  
-
-  // UNHIDE SIGNUP/LOGIN FORMS  
+  // UNHIDE SIGNUP/LOGIN FORMS
   $('#nav-login').on('click', function() {
     //  remove class of hidden from the login/signup forms
     $('.account-form').removeClass('hidden');
@@ -97,70 +99,6 @@ $(function() {
       });
     });
   });
-
-
-
-  $('#nav-submit').on('click', function() {
-    $('#submit-form').removeClass('hidden');
-  });
-
-  $('#newStory').on('click', function(e) {
-    //e.preventDefault(); // anytime you have a form & not reload page on click
-    let author = $('#author').val();
-    let title = $('#title').val();
-    let url = $('#url').val();
-    stories.addStory(currentUser, { title, author, url }, function(newStory) {
-      console.log(newStory);
-    });
-  });
-}); //  end of window
-
-//  business logic-y - still in global scope
-function showNavForLoggedInUser() {
-  $('#nav-login').hide();
-  $('#user-profile').hide();
-  $('.main-nav-links, #user-profile').toggleClass('hidden');
-  $('#nav-welcome').show();
-  $('#nav-logout').show();
-}
-
-let newPassword = $('#create-account-password').val();
-  User.create(newUserName, newPassword, newAccountName, function(newUser) {
-    currentUser = newUser;
-    showNavForLoggedInUser();
-  })
-});
-
-  $('#login').on('click', function(e) {
-    e.preventDefault(); // anytime you have a form & not reload page on click
-    let currentUserName = $('#login-username').val();
-    let currentPassword = $('#login-password').val();
-
-    currentUser = new User(currentUserName, currentPassword);
-    currentUser.login(function loginAndSubmitForm(updatedUser) {
-      currentUser = updatedUser;
-      currentUser.retrieveDetails(() => {
-        token = currentUser.loginToken;
-        $('#login-form').hide();
-        $('#create-account-form').hide();
-        showNavForLoggedInUser();
-        //  ***QUESTION - how to set this item in local storage?
-        localStorage.setItem('token', currentUser.loginToken);
-        localStorage.setItem('username', currentUser.username);
-      });
-    });
-
-    //  { user: { username: this.username, password: this.password } }
-  });
-
-  //  on the login, will need to store the token and the
-  //  currentUser
-
-  //  As a logged in user, I can create a new story.
-
-  //  user fills out each field
-  //  we grab each input value
-  //  when user click on submit BUTTON, post the form input
 
   $('#nav-submit').on('click', function() {
     $('#submit-form').removeClass('hidden');
